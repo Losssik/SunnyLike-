8bc1395a7aa19e1d422b1dea6baaf1ddec524700
Losssik <przemek.bialkwno@gmail.com>
2022-10-21 17:54:01 +0200

    getting and rendering weather data

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
-//const getData = function () {
-//fetch(
-//"api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=4eb94309c7891a62437268af9921df5b"
-//)
-//.then((response) => response.json())
-//.then((data) => console.log(data[0]));
-//};
+"use strict";
 
-//getData();
+const tescik = document.querySelector(".main__header");
+const weatherCard = document.querySelector(".weather__card");
+const maxTemperature = document.querySelector(".weather__maxtemp");
+const minTemperature = document.querySelector(".weather__mintemp");
+const wind = document.querySelector(".weather__wind");
 
-const testPromise = async function () {
-  const res = await fetch(
-    "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=14b138fd386ea13bf4ae40177706e7a5"
-  );
-  const data = await res.json();
-  console.log(data);
-  console.log(data.list[0].main);
+const updateWeather = function (data) {
+  tescik.textContent = data.city.name;
+  //converting from kelvin to celsius
+  maxTemperature.textContent = `${(data.list[0].main.temp_max - 273.15).toFixed(
+    1
+  )} 'C`;
+  minTemperature.textContent = `${(data.list[0].main.temp_min - 273.15).toFixed(
+    1
+  )} 'C  `;
+  //converting from km/h to m/s
+  wind.textContent = `${((data.list[0].wind.speed / 1000) * 3600).toFixed(
+    1
+  )} km/h`;
+
+  //showing card
+  weatherCard.style.opacity = 1;
 };
-testPromise();
+
+const request = fetch(
+  "https://api.openweathermap.org/data/2.5/forecast?lat=54.51&lon=18.53&appid=14b138fd386ea13bf4ae40177706e7a5"
+);
+
+const getWeatherInfo = function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=14b138fd386ea13bf4ae40177706e7a5"
+  )
+    .then((resposne) => resposne.json())
+    .then(function (data) {
+      console.log(data);
+      updateWeather(data);
+    });
+};
+
+getWeatherInfo();
